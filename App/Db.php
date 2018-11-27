f2a8f8d7a1753615155da6b7dd53df06fbc2b94b
Andrey Rogatin <AndreyR-Profit@yandex.ru>
2018-11-27 14:02:58 +0300

    update class Db

--- App/Db.php
+++ App/Db.php
@@ -5,5 +5,33 @@ namespace App;
 
 class Db
 {
+    protected $dbh;
 
+    public function __construct()
+    {
+        $dsn = 'mysql:host=localhost;dbname=php2';
+        $this->dbh = new \PDO($dsn, 'root', '');
+    }
+
+    public function query(string $sql, array $data, string $class = '')
+    {
+        $sth = $this->dbh->prepare($sql);
+        $sth->execute($data);
+        $res = $sth->fetchAll(\PDO::FETCH_ASSOC);
+
+        if (empty($class)) {
+            return $res;
+        }
+
+        $arr = [];
+
+        foreach ($res as $row) {
+            $obj = new $class;
+            foreach ($row as $key => $value) {
+                $obj->$key = $value;
+            }
+            $arr[] = $obj;
+        }
+        return $arr;
+    }
 }
--- App/index.php
+++ App/index.php
@@ -1,3 +1,9 @@
 <?php
 
 require __DIR__ . '/autoload.php';
+
+$db = new \App\Db;
+
+$res = $db->query('SELECT * FROM persons', []);
+
+var_dump($res);
